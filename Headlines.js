fee8b4c686e090f581a2d2cae5d190da9979f50b
joshua londono <joshualondono@outlook.com>
2020-12-07 11:05:24 -0500

    first commit

---
+++ Headlines.js
@@ -0,0 +1,36 @@
+import React from 'react';
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('a63f62608ebf43d4b855f19cde053677');
+
+export default class Headlines extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    newsapi.v2.everything({
+      q: 'trump',
+      sources: 'cnn',
+    //domains: test.toString(),
+    // from: '2017-12-01',
+      // to: '2017-12-12',
+      language: 'en',
+      sortBy: 'relevancy',
+      page: 2
+    }).then(response => {
+      const persons = response.articles.map(x => [x.description, x.url, x.publishedAt]);
+    
+      console.log(persons)
+    
+    
+    });
+  }
+
+  render() {
+    return (
+      <h2>
+        { alert(this.state.persons)}
+      </h2>
+    )
+  }
+}
